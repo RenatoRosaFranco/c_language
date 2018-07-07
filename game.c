6d263468a48778a2bf8cbe5d09973da9a4bf8bc3
Renato Rosa Franco <Renato_ny@live.com>
2018-07-07 18:55:09 -0300

    Chapter III - Variables

---
+++ game.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main(){
+  printf("************************************\n");
+  printf("* Bem vindo ao Jogo de Adivinhação *\n");
+  printf("************************************\n");
+
+	int numerosecreto;
+	numerosecreto = 42;
+
+	printf("O numero secreto é %d \n", numerosecreto);
+}
