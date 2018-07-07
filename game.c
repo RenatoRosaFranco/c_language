438d5bf4331bfb17a17e7d21f9d2f66ac1eeac50
Renato Rosa Franco <Renato_ny@live.com>
2018-07-07 18:59:05 -0300

    Chapter IV - Control flow [if and fors]

--- game.c
+++ game.c
@@ -5,8 +5,12 @@ int main(){
   printf("* Bem vindo ao Jogo de Adivinhação *\n");
   printf("************************************\n");
 
-	int numerosecreto;
+	int numerosecreto, chute;
 	numerosecreto = 42;
 
+	printf("Qual é o seu chute? \n");
+	scanf("%d", &chute);	
+
+	printf("Você chutou o numero %d \n", chute);
 	printf("O numero secreto é %d \n", numerosecreto);
 }
