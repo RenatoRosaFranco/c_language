cfbbbb06964a2eb403bbd65a50a50c0c8cb84ad5
Renato Rosa Franco <Renato_ny@live.com>
2018-07-07 19:53:40 -0300

    Introduction to define

--- game.c
+++ game.c
@@ -1,4 +1,5 @@
 # include <stdio.h>
+# define NUMERO_DE_TENTATIVAS 3
 
 int main(){
   printf("************************************\n");
@@ -8,9 +9,31 @@ int main(){
 	int numerosecreto, chute;
 	numerosecreto = 42;
 
-	printf("Qual é o seu chute? \n");
-	scanf("%d", &chute);	
+	for(int i= 0; i <= NUMERO_DE_TENTATIVAS; i++){
+		printf("Qual é o seu chute? \n");
+		scanf("%d", &chute);	
 
-	printf("Você chutou o numero %d \n", chute);
-	printf("O numero secreto é %d \n", numerosecreto);
+		printf("Você chutou o numero %d \n", chute);
+		
+		int acertou = chute == numerosecreto;
+
+		if (acertou){
+			printf("Parabéns você acertou!\n");
+			printf("Jogue denovo, você é um bom jogador!\n");
+			break;
+		}
+		else {
+
+			// se o numero for maior
+			int maior = chute > numerosecreto;
+			if (maior) {
+				printf("Seu chute foi maior que o numero secreto!\n");
+			}
+			else {	
+				printf("Seu chute foi menor do que o numero secreto!\n");
+			}
+			printf("Mas não desanime, tente denovo!\n");
+		}
+	}
+	printf("Fim de jogo!\n");
 }
