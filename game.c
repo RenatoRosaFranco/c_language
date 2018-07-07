1c21f2290e8dc7b8151c95783106d1c65768a86c
Renato Rosa Franco <Renato_ny@live.com>
2018-07-07 20:08:14 -0300

    Refactoring C code

--- game.c
+++ game.c
@@ -10,30 +10,36 @@ int main(){
 	numerosecreto = 42;
 
 	for(int i= 0; i <= NUMERO_DE_TENTATIVAS; i++){
+		printf("Numero de tentativas %d \n", NUMERO_DE_TENTATIVAS);
+		printf("Tentativas usadas %d \n\n", i);
 		printf("Qual é o seu chute? \n");
 		scanf("%d", &chute);	
 
 		printf("Você chutou o numero %d \n", chute);
 		
-		int acertou = chute == numerosecreto;
-
-		if (acertou){
-			printf("Parabéns você acertou!\n");
-			printf("Jogue denovo, você é um bom jogador!\n");
-			break;
+		if (chute < 0 ) {
+			printf("Você não pode chutar numeros negativos \n");
+			i--;
+			continue;
 		}
 		else {
-
-			// se o numero for maior
+			int acertou = chute == numerosecreto;
 			int maior = chute > numerosecreto;
-			if (maior) {
+
+			if (acertou){
+				printf("Parabéns você acertou!\n");
+				printf("Jogue denovo, você é um bom jogador!\n");
+				break;
+			}
+			else if(maior) {
+				// se o numero for maior
 				printf("Seu chute foi maior que o numero secreto!\n");
 			}
 			else {	
 				printf("Seu chute foi menor do que o numero secreto!\n");
 			}
-			printf("Mas não desanime, tente denovo!\n");
+			printf("Mas não desanime, tente denovo!\n\n");
 		}
+	printf("Fim de jogo!\n\n");
 	}
-	printf("Fim de jogo!\n");
 }
